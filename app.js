0222468f4d14134d06dc47a4f1ba1c9572453357
Mateus <abdallahmateus@gmail.com>
2022-06-26 21:22:19 -0300

    setting functions in the buttons

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const btns = document.querySelectorAll('.question-btn');
+const questions = document.querySelectorAll('.question');
+
 
 btns.forEach(function(btn){
     btn.addEventListener('click', function(e){
-        console.log(e.currentTarget);
-    })
-})
+        const question = e.currentTarget.parentElement.parentElement;
+        question.classList.toggle('show-text');
+    });
+});
+
